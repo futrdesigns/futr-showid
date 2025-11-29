51546bda54456d2fde7d610b5a51c9f7bb2249b5
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-11-29 20:10:48 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,20 +1,23 @@
 Config = {}
 
 -- Notification system to use
--- Options: 'qb', 'okok', 'mythic'
-Config.NotificationType = 'qb'
+-- Options: 'ox', 'qb', 'okok', 'mythic'
+Config.NotificationType = 'ox'
 
 -- Show animation when checking ID
 Config.ShowAnimation = true
 
 -- Show floating text above players' heads with their ID
-Config.ShowFloatingText = false
+Config.ShowFloatingText = true
 
 -- Enable /myid command and keybind
 Config.EnableKeybind = true
 
 -- Keybind for showing your own ID (default: F5)
 Config.Keybind = 'F5'
+
+-- Show keybind help text in console occasionally
+Config.ShowKeybindHelp = true
 
 -- Enable debug mode (prints to console)
 Config.Debug = false
