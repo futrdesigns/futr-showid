bf44eebca743e4768a3917f6db91742dba55d21f
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-11-29 20:12:07 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local PlayerData = {}
 local lastTargetTime = 0
-local cooldown = 1000 -- 1 second cooldown
+local cooldown = 1000
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     PlayerData = QBCore.Functions.GetPlayerData()
@@ -20,7 +20,7 @@ function PlayIDAnimation()
     end
     
     TaskPlayAnim(ped, "mp_common", "givetake1_a", 8.0, -8.0, -1, 0, 0, false, false, false)
- 
+
     Citizen.CreateThread(function()
         Wait(2000)
         StopAnimTask(ped, "mp_common", "givetake1_a", 1.0)
@@ -28,73 +28,98 @@ function PlayIDAnimation()
 end
 
 function ShowNotification(msg, type)
-    if Config.NotificationType == 'qb' then
-        QBCore.Functions.Notify(msg, type or 'primary', 5000)
+    if Config.NotificationType == 'ox' then
+        lib.notify({
+            title = 'ID System',
+            description = msg,
+            type = type or 'inform',
+            position = 'top'
+        })
     elseif Config.NotificationType == 'okok' then
-        exports['okokNotify']:Alert("ID System", msg, 5000, type or 'info')
+        exports['okokNotify']:Alert("ID SYSTEM", msg, 5000, type or 'info')
     elseif Config.NotificationType == 'mythic' then
         exports['mythic_notify']:DoHudText('inform', msg)
+    elseif Config.NotificationType == 'qb' then
+        QBCore.Functions.Notify(msg, type or 'primary', 5000)
     else
-        -- Default QB
-        QBCore.Functions.Notify(msg, 'primary', 5000)
+        lib.notify({
+            title = 'ID System',
+            description = msg,
+            type = type or 'inform',
+            position = 'top'
+        })
     end
 end
 
-exports['qb-target']:AddGlobalPlayer({
-    options = {
-        {
-            type = "client",
-            icon = 'fas fa-id-card',
-            label = 'Show Player ID',
-            action = function(entity)
-                local currentTime = GetGameTimer()
+exports.ox_target:addGlobalPlayer({
+    {
+        name = 'show_player_id',
+        icon = 'fas fa-id-card',
+        label = 'Show Player ID',
+        distance = 2.5,
+        onSelect = function(data)
+            local currentTime = GetGameTimer()
+
+            if currentTime - lastTargetTime < cooldown then
+                ShowNotification("Please wait before checking another ID", 'error')
+                return
+            end
+            
+            lastTargetTime = currentTime
+            
+            local targetEntity = data.entity
+            
+            if DoesEntityExist(targetEntity) and IsEntityAPed(targetEntity) then
+                local playerId = GetPlayerServerId(NetworkGetEntityOwner(targetEntity))
                 
-                -- Cooldown check
-                if currentTime - lastTargetTime < cooldown then
-                    ShowNotification("Please wait before checking another ID", 'error')
-                    return
-                end
-                
-                lastTargetTime = currentTime
-                
-                if DoesEntityExist(entity) and IsEntityAPed(entity) then
-                    local playerId = GetPlayerServerId(NetworkGetEntityOwner(entity))
-                    
-                    if playerId > 0 then
-                        -- Play animation
-                        if Config.ShowAnimation then
-                            PlayIDAnimation()
-                        end
-                        
-                        -- Show ID notification
-                        ShowNotification("Player ID: " .. playerId, 'success')
-                        
-                        -- Debug message
-                        if Config.Debug then
-                            print("[ShowID] Displayed ID: " .. playerId)
-                        end
-                    else
-                        ShowNotification("Invalid player targeted", 'error')
+                if playerId > 0 and playerId ~= GetPlayerServerId(PlayerId()) then
+                    if Config.ShowAnimation then
+                        PlayIDAnimation()
+                    end
+
+                    ShowNotification("Player ID: " .. playerId, 'success')
+                   
+                    if Config.Debug then
+                        print("[ShowID] Displayed ID: " .. playerId)
                     end
                 else
-                    ShowNotification("No valid player targeted", 'error')
+                    ShowNotification("Cannot check your own ID", 'error')
                 end
-            end,
-            canInteract = function(entity, distance, data)
-                return not IsPedAPlayer(entity) or (IsPedAPlayer(entity) and GetPlayerServerId(NetworkGetEntityOwner(entity)) ~= GetPlayerServerId(PlayerId()))
+            else
+                ShowNotification("No valid player targeted", 'error')
             end
-        }
-    },
-    distance = 2.5
+        end,
+        canInteract = function(entity, distance, data)
+            if not IsPedAPlayer(entity) then return false end
+            local targetPlayerId = GetPlayerServerId(NetworkGetEntityOwner(entity))
+            return targetPlayerId ~= GetPlayerServerId(PlayerId())
+        end
+    }
 })
 
 RegisterCommand('myid', function()
     local playerId = GetPlayerServerId(PlayerId())
-    ShowNotification("Your Player ID: " .. playerId, 'primary')
+    ShowNotification("Your Player ID: " .. playerId, 'inform')
+    
+    if Config.Debug then
+        print("[ShowID] My ID: " .. playerId)
+    end
 end, false)
+
+TriggerEvent('chat:removeSuggestion', '/myid')
+TriggerEvent('chat:addSuggestion', '/myid', 'Show your own player ID', {})
 
 if Config.EnableKeybind then
     RegisterKeyMapping('myid', 'Show Your Player ID', 'keyboard', Config.Keybind)
+
+    Citizen.CreateThread(function()
+        while true do
+            Wait(10000)
+            if Config.ShowKeybindHelp then
+                print("[ShowID] Press " .. Config.Keybind .. " or type /myid to see your player ID")
+            end
+        end
+    end)
 end
 
 if Config.ShowFloatingText then
@@ -112,12 +137,29 @@ if Config.ShowFloatingText then
                     sleep = 0
                     local playerId = GetPlayerServerId(player)
                     local text = "ID: " .. playerId
-                    
-                    exports['qb-core']:DrawText3D(targetCoords.x, targetCoords.y, targetCoords.z + 1.0, text)
+
+                    lib.drawText3D({
+                        coords = vector3(targetCoords.x, targetCoords.y, targetCoords.z + 1.0),
+                        text = text,
+                        size = 0.3,
+                        color = {255, 255, 255, 255}
+                    })
                 end
             end
             
             Wait(sleep)
         end
     end)
+end
+
+if Config.Debug then
+    RegisterCommand('showid_debug', function()
+        print("=== ShowID Debug Information ===")
+        print("Player ID: " .. GetPlayerServerId(PlayerId()))
+        print("Resource Name: " .. GetCurrentResourceName())
+        print("OX Target Available: " .. tostring(exports.ox_target ~= nil))
+        print("QB Core Available: " .. tostring(exports['qb-core'] ~= nil))
+        print("Notification Type: " .. Config.NotificationType)
+        print("=================================")
+    end, false)
 end
